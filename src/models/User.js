ef29fdb80c4fb6a38dee3f108c01e78c91901e53
Harrison <monteiro4100@gmail.com>
2023-01-26 18:43:31 -0300

    requisito 2

--- src/models/User.js
+++ src/models/User.js
@@ -1,7 +1,8 @@
 
 
 const userModel = (sequelize, DataTypes) => {
-    const user = sequelize.define('user', {
+    const user = sequelize.define('User', {
+        id: DataTypes.INTEGER,
         display_name: DataTypes.STRING,
         email: DataTypes.STRING,
         password: DataTypes.STRING,
