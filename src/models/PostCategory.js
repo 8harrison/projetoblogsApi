b85ba7162b247afcf12b1e7f6863ddc70fc40813
Harrison <monteiro4100@gmail.com>
2023-02-01 12:25:23 -0300

    requisito 11

---
+++ src/models/PostCategory.js
@@ -0,0 +1,42 @@
+const postCategoryModel = (sequelize, DataTypes) => {
+  const post = sequelize.define(
+    "PostCategory",
+    {
+      postId:{
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+      } ,
+      categoryId:{
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+      } 
+    },
+    {
+      underscored: true,
+      tableName: "post_categories",
+      timestamps: false,
+    }
+  );
+  post.associate = (models) => {
+    models.BlogPost.belongsToMany(models.Category, {
+    //   foreignKey: "post_id",
+      as: "categoryPost",
+      through: post,
+      foreignKey: "postId",
+      otherKey: "categoryId",
+    });
+    // post.associate = (models) => {
+        models.Category.belongsToMany(models.BlogPost, {
+        //   foreignKey: "post_id",
+          as: "posts",
+          through: post,
+          foreignKey: "categoryId",
+          otherKey: "postId",
+        });
+    //   };
+    }
+      
+  return post;
+};
+
+module.exports = postCategoryModel;
